73b37a3432a1c18f9715e972ca67f6cb8d87e551
ianthomas2200 <ianthomas2200@gmail.com>
2016-10-18 14:06:56 +0100

    Add files via upload

---
+++ TestFramework/Pages.cs
@@ -0,0 +1,42 @@
+﻿using log4net;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+
+namespace TestFramework
+{
+    public static class Pages
+    {
+        public static class HomePage
+        {
+            static IWebDriver driver = new ChromeDriver(@"C:\TestTools");
+            public static ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            public static void Goto(string URL1)
+            {
+                log.Error("----------");
+                driver.Url = URL1;
+                driver.Manage().Window.Maximize();
+                Thread.Sleep(2000);
+            }
+
+            public static bool IsAt()
+            {
+                string requiredpagetitle = "Pluralsight | Unlimited Online Developer, IT and Creative Training";
+                string pagetitle = driver.Title;
+                log.Info("test");
+                log.Info(pagetitle);
+                return requiredpagetitle == driver.Title;
+            }
+
+            public static void Close()
+            {
+                driver.Close();
+            }
+
+        }
+    }
+
+}
---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestFramework;
+
+
+namespace Tests
+{
+    [TestClass]
+    public class OverallTests
+    {
+        public string URL = "https://www.pluralsight.com/";
+
+        [TestMethod]
+        public void Go_to_Home_page()
+        {
+            Pages.HomePage.Goto(URL);
+            Assert.IsTrue(Pages.HomePage.IsAt());
+        }
+
+        [TestCleanup]
+        public  void TidyUPATEND()
+        {
+            Pages.HomePage.Close();
+        }
+    }
+}
